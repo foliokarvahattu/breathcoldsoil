e0fde8eaf2aa1954ccb9fea9a1672412f008b082
Elias <Elias@192.168.11.7>
2018-03-30 15:19:36 +0300

    P

---
+++ breathcoldsoil/breathcoldsoil/magic.py
@@ -0,0 +1,16 @@
+'''
+Created on 28 Mar 2018
+
+@author: Elias
+'''
+
+class Magic:
+
+
+
+    def __init__(self, name):
+        self.name = name
+        self.damage = 0
+        self.description = ""
+        self.effects = []
+        
